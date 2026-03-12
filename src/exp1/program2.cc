2bccf37c00253bd3b599ab16850c656ceb657724
Amol Yadav <amyssnipet@yahoo.com>
2026-03-12 15:19:07 +0530

    updates exp1 program 2

--- src/exp1/program2.cc
+++ src/exp1/program2.cc
@@ -7,18 +7,14 @@ public:
     virtual void calculateAndDisplayArea() const {
         cout << "Area of generic shape\n";
     }
-    
     virtual ~Shape() {}
 };
 
 class Rectangle : public Shape {
 private:
-    float length;
-    float width;
-
+    float length, width;
 public:
     Rectangle(float l, float w) : length(l), width(w) {}
-
     void calculateAndDisplayArea() const override {
         cout << "Rectangle Area: " << (length * width) << "\n";
     }
@@ -27,19 +23,25 @@ public:
 class Circle : public Shape {
 private:
     float radius;
-
 public:
     Circle(float r) : radius(r) {}
-
     void calculateAndDisplayArea() const override {
         cout << "Circle Area: " << (3.14159f * radius * radius) << "\n";
     }
 };
 
 int main() {
-    Shape* shapePointer1 = new Rectangle(5.0f, 4.0f);
-    Shape* shapePointer2 = new Circle(3.0f);
+    float r, l, w;
 
+    cout << "Enter length and width of the rectangle: ";
+    cin >> l >> w;
+    Shape* shapePointer1 = new Rectangle(l, w);
+
+    cout << "Enter radius of the circle: ";
+    cin >> r;
+    Shape* shapePointer2 = new Circle(r);
+
+    cout << "\n  Results   \n";
     shapePointer1->calculateAndDisplayArea();
     shapePointer2->calculateAndDisplayArea();
 
